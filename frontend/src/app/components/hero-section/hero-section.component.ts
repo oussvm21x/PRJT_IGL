df219fa281cc01775343fae2c6051a255f8aaac4
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-12-14 16:09:29 +0100

    Hero , hada win bdit

---
+++ frontend/src/app/components/hero-section/hero-section.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [CommonModule],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.css'
+})
+export class HeroSectionComponent {
+
+}
--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "../hero-section/hero-section.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [HeroSectionComponent, CommonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
