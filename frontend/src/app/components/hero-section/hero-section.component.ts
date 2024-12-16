c67b8e9d30f361874d26bee355962b983c0351f6
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-12-16 15:16:36 +0100

    heroSection :)

--- frontend/src/app/components/hero-section/hero-section.component.ts
+++ frontend/src/app/components/hero-section/hero-section.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-hero-section',
-  imports: [CommonModule],
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './hero-section.component.html',
   styleUrl: './hero-section.component.css'
 })
