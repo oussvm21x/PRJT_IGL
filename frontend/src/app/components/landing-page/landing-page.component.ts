bb5d4a493ea1af72415b57405414cd4ea9da7029
Khelifa Saadallah Oussama <oussakhema00@gmail.com>
2024-12-30 21:02:55 +0100

    nos-user

--- frontend/src/app/components/landing-page/landing-page.component.ts
+++ frontend/src/app/components/landing-page/landing-page.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NosUserComponent } from "../nos-user/nos-user.component";
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [NosUserComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
