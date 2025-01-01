d45880ba094d2cc044f49a175b4f34f52919a345
Ahmed Yacine Allag <ma_allag@esi.dz>
2025-01-01 11:36:11 +0100

    add the qr code functions and updated the urls for easier routing

--- backend/patient/urls.py
+++ backend/patient/urls.py
@@ -4,6 +4,6 @@ from .views import CreatePatientView, GetPatientDetailsView, PatientByNSSView , 
 urlpatterns = [
     path('api/', CreatePatientView.as_view(), name='create_patient'),
     path('api/<str:num_securite_sociale>/', GetPatientDetailsView.as_view(), name='get_patient_details'),
-    path('patients/nss/<str:nss>/', PatientByNSSView.as_view(), name='patient-by-nss'),
-    path('patients/qr-code/<str:qr_code>/', SearchPatientByQRCode.as_view(), name='patient-by-qr-code'),
+    path('api/nss/<str:nss>/', PatientByNSSView.as_view(), name='patient-by-nss'),
+    path('api/qr-code/<str:qr_code>/', SearchPatientByQRCode.as_view(), name='patient-by-qr-code'),
 ]
