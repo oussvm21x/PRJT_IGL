3dd78bb34d6c26a54888cee38a67b2ee2e2a3ed4
Bouali Rima <mr_bouali@esi.dz>
2025-01-11 10:05:50 +0100

    3

--- frontend/src/app/components/radio/layout-radio/layout-radio.component.ts
+++ frontend/src/app/components/radio/layout-radio/layout-radio.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { SidebarLabComponent } from '../sidebar-lab/sidebar-lab.component';
+import { RouterOutlet,Router } from '@angular/router';
 
 @Component({
-  selector: 'app-layout-radio',
-  imports: [],
-  templateUrl: './layout-radio.component.html',
-  styleUrl: './layout-radio.component.css'
+  selector: 'app-layout-lab',
+  imports: [
+        SidebarLabComponent,
+        RouterOutlet
+      
+    ],
+  templateUrl: './layout-lab.component.html',
+  styleUrl: './layout-lab.component.css'
 })
-export class LayoutRadioComponent {
 
+export class LayoutLabComponent implements OnInit{
+  activeRoute: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe(() => {
+      this.activeRoute = this.router.url;
+    });
+  }
 }
---
+++ frontend/src/app/components/radio/sidebar-radio/sidebar-radio.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-radio',
+  imports: [],
+  templateUrl: './sidebar-radio.component.html',
+  styleUrl: './sidebar-radio.component.css'
+})
+export class SidebarRadioComponent {
+
+}
