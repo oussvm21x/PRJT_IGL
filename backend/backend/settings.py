e2aaa0d0be0819988c77fd6192f3840f28e6cf95
yacineallag19 <ma_allag@esi.dz>
2025-01-29 17:03:40 +0100

    Changes to allow sending requests

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'shared_models',
     'consultations',
     'authentication',
@@ -63,11 +64,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200/",
-] 
+CORS_ALLOW_ALL_ORIGINS = True 
 
 ROOT_URLCONF = 'backend.urls'
 
