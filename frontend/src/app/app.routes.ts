45a59dacf223f3ea31d029362322b991de7f8d70
Bouali Rima <mr_bouali@esi.dz>
2025-01-29 19:03:32 +0100

    Revert "Rima/radio"

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
-import { DashboardRadioComponent } from './components/radio/dashboard-radio/dashboard-radio.component';
-import { RadiosComponent } from './components/radio/radios/radios.component';
-import {NotificationsRadioComponent  } from './components/radio/notifications-radio/notifications-radio.component';
-import {ParametresRadioComponent  } from './components/radio/parametres-radio/parametres-radio.component';
-import {LayoutRadioComponent  } from './components/radio/layout-radio/layout-radio.component';
 import { DashboardMdcnComponent } from './components/medecin/dashboard-mdcn/dashboard-mdcn.component';
 import { PatientsComponent } from './components/medecin/patients/patients.component';
 import { NotificationsComponent } from './components/medecin/notifications/notifications.component';
 import { ParametresComponent } from './components/medecin/parametres/parametres.component';
 import { LayoutComponent } from './components/medecin/layout/layout.component';
 import { ModifierPatientComponent } from './components/medecin/modifier-patient/modifier-patient.component';
+
 import { DashboardLabComponent } from './components/laborantin/dashboard-lab/dashboard-lab.component';
 import { BilansComponent } from './components/laborantin/bilans/bilans.component';
 import {NotificationsLabComponent  } from './components/laborantin/notifications-lab/notifications-lab.component';
@@ -33,19 +29,6 @@ export const routes: Routes = [
         
         
     },
-     {
-        path: 'radiologue',
-        component:LayoutRadioComponent,
-        children: [
-            { path: 'dashboardradio', component: DashboardRadioComponent },
-            { path: 'radios', component: RadiosComponent }, 
-            { path: 'notifications', component: NotificationsRadioComponent }, 
-            { path: 'parametres', component: ParametresRadioComponent }, 
-        ],
-
-        
-        
-    },
     {
         path: 'laborantin',
         component:LayoutLabComponent,
@@ -60,6 +43,7 @@ export const routes: Routes = [
         
     },
     { path: '', redirectTo: '/laborantin/dashboardLab', pathMatch: 'full' },
+
 ];
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
@@ -67,3 +51,4 @@ export const routes: Routes = [
   })
   export class AppRoutingModule {}
 
+  
