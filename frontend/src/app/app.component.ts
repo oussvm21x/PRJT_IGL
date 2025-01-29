ff4eb5a27848d8446636480b7694fb98866721e2
Bouali Rima <mr_bouali@esi.dz>
2025-01-29 17:13:49 +0100

    Revert "Oussama/pharma"

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { DashboardMdcnComponent } from './components/pharmacie/dashboard-mdcn/dashboard-mdcn.component';
-import { SidebarComponent } from './components/pharmacie/sidebar/sidebar.component';
 import { DashboardMdcnComponent } from './components/infermier/dashboard-mdcn/dashboard-mdcn.component';
 import { SidebarComponent } from './components/infermier/sidebar/sidebar.component';
 import { LandingPageComponent } from "./components/landing-page/landing-page.component";
@@ -10,11 +8,8 @@ import { DashboardMdcnComponent } from './components/medecin/dashboard-mdcn/dash
 import { SidebarComponent } from './components/medecin/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
-  imports: [
-  RouterOutlet,
-  DashboardMdcnComponent,
-  SidebarComponent,LandingPageComponent, NosChiffresComponent
-  ],
+  imports: [RouterOutlet, LandingPageComponent, NosChiffresComponent,DashboardMdcnComponent,
+  SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
