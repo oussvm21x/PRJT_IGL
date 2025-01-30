a847375e47d569de03e5fb73bea5554bea1734cd
Bouali Rima <mr_bouali@esi.dz>
2025-01-30 01:25:50 +0100

    sans login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes,RouterModule } from '@angular/router';
 
+import { DashboardpharmaComponent } from './components/pharmacie/dashboard-mdcn/dashboard-mdcn.component';
+import { NotificationspharmaComponent } from './components/pharmacie/notifications/notifications.component';
+import { ParametrespharmaComponent } from './components/pharmacie/parametres/parametres.component';
+import { LayoutpharmaComponent } from './components/pharmacie/layout/layout.component';
+import { RapportsComponent } from './components/rapports/rapports.component';
+import { StocksComponent } from './components/stocks/stocks.component';
+import { OrdonnanacesComponent } from './components/ordonnanaces/ordonnanaces.component';
+
+
 import { DashboardMdcnComponent } from './components/medecin/dashboard-mdcn/dashboard-mdcn.component';
 import { PatientsComponent } from './components/medecin/patients/patients.component';
 import { NotificationsComponent } from './components/medecin/notifications/notifications.component';
@@ -55,6 +64,20 @@ export const routes: Routes = [
         path: 'landing',
         component:LandingPageComponent,
     
+    },
+    {
+        path: 'pharmacien',
+        component: LayoutpharmaComponent,
+        children: [
+            { path: 'dashboard', component: DashboardpharmaComponent },
+            { path: 'ordonnances', component: OrdonnanacesComponent },
+            { path: 'stocks', component: StocksComponent },
+            { path: 'rapports', component: RapportsComponent },
+            { path: 'notifications', component: NotificationspharmaComponent },
+            { path: 'parametres', component: ParametrespharmaComponent },
+
+        ],
+
     },
     {
         path: 'infirmier',
